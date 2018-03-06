12506961acd4de6839a502ac003bce46b8d13c99
Serzantor <35774118+Serzantor@users.noreply.github.com>
2018-03-06 11:19:26 +0100

    Created Placeholder windows for login and "Proben"

--- DSA_Manager/src/dsa_manager/FXMLDocumentController.java
+++ DSA_Manager/src/dsa_manager/FXMLDocumentController.java
@@ -5,13 +5,22 @@
  */
 package dsa_manager;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -19,8 +28,13 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+    //Component Names/ids
     @FXML
     private Button buttonProbe;
+    @FXML
+    private TextField bla;
+    @FXML 
+    private MenuItem loginAccItem;
     
 //    @FXML
 //    private void handleButtonAction(ActionEvent event) {
@@ -29,8 +43,48 @@ public class FXMLDocumentController implements Initializable {
 //    }
     
     @FXML
+    private void handleLoginItem(ActionEvent event) throws IOException{
+        try {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("LoginDocument.fxml"));
+        /* 
+         * if "fx:controller" is not set in fxml
+         * fxmlLoader.setController(NewWindowController);
+         */
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+        } catch (IOException e) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new Window.", e);
+        }
+    }
+    
+    
+    @FXML
     private void handleProbeButton(ActionEvent event){
-        System.out.println("Lustig");
+        try {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("ProbeDocument.fxml"));
+        /* 
+         * if "fx:controller" is not set in fxml
+         * fxmlLoader.setController(NewWindowController);
+         */
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Proben");
+        stage.setScene(scene);
+        stage.show();
+        } catch (IOException e) {
+            Logger logger = Logger.getLogger(getClass().getName());
+            logger.log(Level.SEVERE, "Failed to create new Window.", e);
+        }
+    }
+    
+    @FXML
+    private void handleTextField(ActionEvent event){
         
     }
     
